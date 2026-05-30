36b7b051936d3e618086cdf70c593400ae8fba1c
White Wizard <jonathanmwanza162@gmail.com>
2026-05-30 15:25:07 +0300

    nex shop

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
      <div className="navbar">
         <div className="nav-logo">
             <img src={logo} alt="logo" />
-            <p>SHOPPER</p>
+            <p>NEX SHOP</p>
         </div>
         <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
         <ul ref={menuRef} className="nav-menu">
